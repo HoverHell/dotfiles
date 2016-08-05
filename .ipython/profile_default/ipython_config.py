245cc45db99b3d9309dfe8ed8ed6f001587aaed6
HoverHell <hoverhell@gmail.com>
2016-08-05 16:58:29 +0300

    ...

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -1,3 +1,9 @@
+# coding: utf8
+
+from __future__ import print_function
+
+import sys
+
 
 # __into_builtin = True
 # import os
@@ -7,6 +13,51 @@
 try:
     from pyaux import madness
 except ImportError:
-    print("XXX: No pyaux")
+    print("E: No pyaux", file=sys.stderr)
 else:
     madness._olt_into_builtin()
+
+
+c = get_config()
+
+
+try:
+    import Cython as __tmp
+except Exception:
+    pass
+else:
+    print("I: Adding Cython extension", file=sys.stderr)
+    c.InteractiveShellApp.extensions.append('Cython')
+
+
+c.TerminalIPythonApp.display_banner = False
+
+try:
+    from IPython.terminal.prompts import Prompts
+except Exception as _exc:
+    print("W: No 5.x prompts:", _exc, file=sys.stderr)
+else:
+
+    class MyPrompts(Prompts):
+        """ http://ipython.readthedocs.io/en/stable/config/details.html#custom-prompts """
+
+        def continuation_prompt_tokens(self, *args, **kwargs):
+            return []
+
+
+    c.TerminalInteractiveShell.prompts_class = MyPrompts
+
+    def _hax(*args, **kwargs):
+        print("I: Patching the layout", args, kwargs, file=sys.stderr)
+        result = _hax._wrapped(*args, **kwargs)
+        # Attempt to make the long lines input copypasteable.
+        # Problem: it still inserts line breaks at the terminal width.
+        result['multiline'] = False
+        return result
+
+    from IPython.terminal.interactiveshell import TerminalInteractiveShell
+    _hax._wrapped = TerminalInteractiveShell._layout_options
+    TerminalInteractiveShell._layout_options = _hax
+
+
+# c.PrefilterManager.multi_line_specials = False
