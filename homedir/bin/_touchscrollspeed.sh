7903c1771acb01cee1217fa15bb5652b9bb72ba7
hoverhell <hoverhell@gmail.com>
2021-06-11 19:57:43 +0300

    xinput natural scrolling for the low-feature drivers

--- homedir/bin/_touchscrollspeed.sh
+++ homedir/bin/_touchscrollspeed.sh
@@ -11,7 +11,13 @@ id="$(
     xinput list | \
     sed -rn 's/.*([Tt]ouch|DLL07A0)[^=]*id=([0-9]+).*/\2/ p'
 )"
-xinput --list-props "$id" | grep "Scrolling Distance"
-prop_id="$(xinput --list-props $id | grep -P ".*[^C][^i][^r][^c][^u][^l][^a][^r]\sScrolling Distance" | grep -Po '\([0-9]+\)' | grep -Po "[0-9]+")"
-prop_xy="$(xinput --list-props $id | grep -P ".*[^C][^i][^r][^c][^u][^l][^a][^r]\sScrolling Distance" | grep -Po '\s+[-+]{0,1}[0-9]+\,\s*[-+]{0,1}[0-9]+' | tr -d '-')"
-exec xinput --set-prop "$id" "$prop_id" "$val" "$val"
+if xinput --list-props "$id" | grep "Scrolling Distance"; then
+    prop_id="$(xinput --list-props $id | grep -P ".*[^C][^i][^r][^c][^u][^l][^a][^r]\sScrolling Distance" | grep -Po '\([0-9]+\)' | grep -Po "[0-9]+")"
+    prop_xy="$(xinput --list-props $id | grep -P ".*[^C][^i][^r][^c][^u][^l][^a][^r]\sScrolling Distance" | grep -Po '\s+[-+]{0,1}[0-9]+\,\s*[-+]{0,1}[0-9]+' | tr -d '-')"
+    xinput --set-prop "$id" "$prop_id" "$val" "$val"
+fi
+
+if xinput --list-props "$id" | grep "Natural Scrolling"; then
+    prop_id="$(xinput --list-props $id | grep -P "Natural Scrolling Enabled \(" | grep -Po '\([0-9]+\)' | grep -Po "[0-9]+")"
+    xinput --set-prop "$id" "$prop_id" "1"
+fi
