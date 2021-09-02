0a6e264b52861321a1932a8f915217cc17ad620d
hoverhell <hoverhell@gmail.com>
2021-09-02 20:00:40 +0300

    deb dependencies as local metapackages

--- auxiliary/deblists.py
+++ auxiliary/deblists.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 
+import datetime
+import os
+import shutil
+import subprocess
 import sys
+import tempfile
 
 
 class Deblists:
@@ -20,6 +25,7 @@ class Deblists:
     ### Interactive shell local system tools ###
 
     # # Shell set:
+    busybox-static
     bash  # in case it isn't there somehow
     bash-completion  # more convenience
     cdargs
@@ -38,6 +44,7 @@ class Deblists:
     moreutils  # pee sponge ...
     rename  # filename-editing
     strace  # executing with details
+    ltrace
     time  # exetuting with timing
     # uuid  # uuid generation. rare.
 
@@ -55,7 +62,8 @@ class Deblists:
     screen  # terminal multiplexer, backgrounder
     htop iotop
     less
-    joe
+    nano
+    joe-jupp
     # zile
     # emacs-nox
     '''
@@ -69,6 +77,10 @@ class Deblists:
     socat  # TCP/UDP debug/tooling
     nmap  # goes as network debug too
     bind9-host  # DNS debug
+    iputils-ping
+    iputils-tracepath
+    mtr-tiny
+    tcpdump
     dnsutils  # `dig`
     openssh-client  # ...
     netcat-openbsd  # ...
@@ -89,16 +101,16 @@ class Deblists:
     shellcheck  pkg-config llvm
     libc6-dev  libffi-dev  libncurses5-dev  libncursesw5-dev  libgdbm-dev
     libreadline-dev  tk-dev
-    libsqlite3-dev  libssl-dev  openssl  libcap-dev  libcurl4-nss-dev  libpq-dev
-    libfreetype6-dev  libpng-dev
+    libsqlite3-dev  openssl
     zlib1g-dev  libbz2-dev  libzip-dev  liblzma-dev  xz-utils
     libxml2-dev  libxmlsec1-dev
-    liblapack-dev  libblas-dev  libboost-all-dev
+    libboost-all-dev
     lua5.2  liblua5.2-dev
     libtbb-dev
 
-    python python-dev python-pip python-pip-whl python-virtualenv
-    python3 python3-dev python3-pip python3-venv
+    libcap-dev libcurl4-nss-dev libpq-dev libfreetype6-dev
+    libpng-dev liblapack-dev libblas-dev libssl-dev
+    python3.9 python3.9-venv python3.9-dev
     '''
 
     file_formats = '''
@@ -163,8 +175,27 @@ class Deblists:
     cups
     eject
     lshw
+    hdparm
     powertop
     fancontrol hddtemp i2c-tools lm-sensors
+    irqbalance
+    lvm2
+    '''
+
+    main_system_ubu = '''
+    # ubuntu-desktop-minimal
+    # ubuntu-desktop
+    kubuntu-desktop
+    kubuntu-restricted-addons
+    linux-generic-hwe-20.04
+    xen-system-amd64
+    '''
+
+    main_system_all = '''
+    hdeps-common hdeps-common-net hdeps-cli hdeps-cli-net
+    hdeps-host-common
+    hdeps-build hdeps-file-formats
+    hdeps-main-system hdeps-main-system-x hdeps-main-system-hw hdeps-main-system-ubu
     '''
 
     dev_max = '''
@@ -209,13 +240,80 @@ def printout():
             for key in sys.argv[1:]
             for pkg in sets[key]
         }
-        print(' '.join(sorted(pkgs)))
+        print('  '.join(sorted(pkgs)))
     else:
         print('\n'.join(
-            name + '    ' + ' '.join(sorted(pkgs))
+            name + ':    ' + ' '.join(sorted(pkgs)) + '\n'
             for name, pkgs in sets.items()
         ))
 
 
-if __name__ == '__main__':
+
+def _run(*args, **kwargs):
+    return subprocess.check_call(args, **kwargs)
+
+
+CONTROL_TPL = """
+Package: hdeps-{name_dash}
+Version: 1.0-{now}
+Architecture: all
+Maintainer: hoverhell <hoverhell@localhost>
+Depends: {deps_comma}
+Priority: optional
+Description: h system dependencies {name_underscore}
+ This package depends on all of the packages
+ listed as '{name_underscore}'
+""".lstrip()
+
+
+def build_one_i(pkg_set_name, target_dir):
+    pkg_set_name_dash = pkg_set_name.replace('_', '-')
+    pkg_name = f'hdeps-{pkg_set_name_dash}'
+    deb_dir = os.path.join(pkg_name, 'DEBIAN')
+    control = CONTROL_TPL.format(
+      name_dash=pkg_set_name_dash,
+      name_underscore=pkg_set_name,
+      now=datetime.datetime.now().strftime('%Y%m%d%H%M%S'),
+      deps_comma=', '.join(DEBSETS[pkg_set_name]),
+    )
+
+    os.mkdir(pkg_name)
+    os.mkdir(deb_dir)
+    with open(os.path.join(deb_dir, 'control'), 'w') as fobj:
+        fobj.write(control)
+    _run('dpkg', '-b', pkg_name)
+    debfile = f'{pkg_name}.deb'
+    assert os.path.exists(debfile), 'should have been created by dpkg'
+    shutil.copy(debfile, os.path.join(target_dir, debfile))
+
+
+def build_one(pkg_set_name):
+    main_dir = os.path.realpath('.')
+    try:
+        with tempfile.TemporaryDirectory(prefix='hdfpb_') as tempdir:
+            os.chdir(tempdir)
+            build_one_i(pkg_set_name, target_dir=main_dir)
+    finally:
+        os.chdir(main_dir)
+
+
+def build():
+    repo_path = '/usr/local/etc/hdeps'
+    cfg_path = '/etc/apt/sources.list.d/hdeps.list'
+    os.makedirs(repo_path, exist_ok=True)
+    os.chdir(repo_path)
+    for pkg_set_name in DEBSETS:
+        build_one(pkg_set_name)
+    _run('sh', '-x', '-c', 'dpkg-scanpackages -m . | gzip > Packages.gz')
+    with open(cfg_path, 'w') as fobj:
+        fobj.write(f'deb [trusted=yes] file:{repo_path} /\n')
+    _run('apt', 'update')
+
+
+def main():
+    build()
     printout()
+
+
+if __name__ == '__main__':
+    main()
